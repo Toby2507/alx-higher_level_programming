25336baf0891f1ad0a70317369dda7a0cf220672
Toby2507 <oluwatobisalau2000@gmail.com>
2023-06-11 11:13:40 +0000

    Fix checker error

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -7,11 +7,14 @@
  */
 void print_python_list_info(PyObject *p)
 {
-	long int i, size = Py_SIZE(p);
-	PyListObject *list = p;
+	long int i, size = PyList_Size(p);
+	PyObject *item;
 
 	printf("[*] Size of the Python List = %ld\n", size);
-	printf("[*] Allocated = %ld\n", list->allocated);
+	printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
 	for (i = 0; i < size; i++)
-		printf("Element %ld: %s\n", i, Py_TYPE(PyList_GetItem(list, i))->tp_name);
+	{
+		item = PyList_GetItem(p, i);
+		printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
+	}
 }
