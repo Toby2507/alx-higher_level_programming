6b00f1f01f4315f3845c0035bce1b3ff3c934792
Toby2507 <oluwatobisalau2000@gmail.com>
2023-06-11 10:27:54 +0000

    Remove integer string casts

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for i, col in enumerate(row):
-            print("{}".format(col), end=' ' if i < len(row) - 1 else '')
+            print("{:d}".format(col), end=' ' if i < len(row) - 1 else '')
         print()
