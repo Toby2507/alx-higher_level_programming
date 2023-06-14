1a8f4cb1bc0acfb34169a0dc28b902bc8acc27e2
Toby2507 <oluwatobisalau2000@gmail.com>
2023-06-14 23:23:22 +0000

    Handle None input

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,5 @@
 #/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    n_mat = matrix[:]
-    return [[row[i] ** 2 for i in range(len(n_mat))] for row in n_mat]
+    if matrix:
+        n_mat = matrix[:]
+        return [[row[i] ** 2 for i in range(len(n_mat))] for row in n_mat]
