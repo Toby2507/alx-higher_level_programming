abd23fd777bb80ae80c755fdf84a104c7c7c1b47
Toby2507 <oluwatobisalau2000@gmail.com>
2023-06-15 01:16:12 +0000

    Handle None input appropriately

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
+    total = 0
     if my_list:
-        total = 0;
         for t in set(my_list):
             total += t
-        return total
+    return total
