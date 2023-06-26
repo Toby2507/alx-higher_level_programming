2df238f40bb0a9a4d5b098cee58786113d2abf92
Toby2507 <oluwatobisalau2000@gmail.com>
2023-06-26 17:11:12 +0000

    Refactor functions to match pycodestyle

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,7 +7,7 @@ def safe_print_list_integers(my_list=[], x=0):
             printed += 1
         except IndexError:
             raise
-        except:
+        except Exception:
             print(end="")
     print()
     return printed
--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -4,7 +4,7 @@ def safe_print_division(a, b):
     try:
         quotient = a / b
         return quotient
-    except:
+    except Exception:
         return quotient
     finally:
         print("Inside result: {}".format(quotient))
