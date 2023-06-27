8175069d9e4f508e0fdb2ca31a95552b91e46bdd
Toby2507 <oluwatobisalau2000@gmail.com>
2023-06-27 23:48:11 +0000

    Add a python module that contains a Square class with limited functionality for now

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This module creates a Square class based on file 0-square.py
+"""
+
+
+class Square:
+    """This is a class called square with very limited functionality"""
+    def __init__(self, size):
+        self.__size = size
