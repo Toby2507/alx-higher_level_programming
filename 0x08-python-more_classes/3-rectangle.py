7d9cc0a5b24727c3981bd43508908b81b385023a
Toby2507 <oluwatobisalau2000@gmail.com>
2023-07-03 16:37:43 +0100

    Add a __str__ method to a python class Rectangle based on 2-rectangle.py

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+class Rectangle:
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+    def __str__(self):
+        if not self.width or not self.height:
+            return ""
+        rectangle = ""
+        for _ in range(self.height):
+            rectangle += '#' * self.width + '\n'
+        return rectangle.rstrip()
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, val):
+        if not isinstance(val, int):
+            raise TypeError("width must be an integer")
+        if val < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = val
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, val):
+        if not isinstance(val, int):
+            raise TypeError("height must be an integer")
+        if val < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = val
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        return (2 * self.width) + (2 * self.height)
