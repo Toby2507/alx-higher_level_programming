cef9286e15e4cc19c87951a41ad7f97dbbbd6fbc
Toby2507 <oluwatobisalau2000@gmail.com>
2023-07-04 21:34:21 +0100

    Add a python file with a locked class

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""contains a Locked class"""
+
+
+class LockedClass:
+    """Lockedclass blueprint"""
+    __slots__ = ['first_name']
+
+    def __init__(self):
+        """initializes class"""
+        pass
