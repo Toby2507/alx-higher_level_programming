13573c8f081d5581d39d1adc1c8e60bcc74ac199
Toby2507 <oluwatobisalau2000@gmail.com>
2023-07-11 00:49:50 +0100

    Add a python file that contains a class Square that inherits from class Rectangle

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Contains a class Square
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square blueprint
+
+    Attributes:
+        size (int): square's width
+
+    Methods:
+        area: computes the area of the square
+    """
+
+    def __init__(self, size):
+        """Initializes the square"""
+        self.integer_validator('size', size)
+        self.__size = size
+
+    def __str__(self):
+        """returns unofficial string representation"""
+        return f"[Rectangle] {self.__size}/{self.__size}"
+
+    def area(self):
+        """computes the area of the square"""
+        return self.__size ** 2
