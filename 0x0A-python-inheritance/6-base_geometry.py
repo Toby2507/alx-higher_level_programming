02b6110159e9c34f346810fb7342a9714ced43a6
Toby2507 <oluwatobisalau2000@gmail.com>
2023-07-11 00:10:01 +0100

    Add a public instance method area that raises an exception when called to the BaseGeometry class

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Contains an empty class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """A blueprint for BaseGeometry
+
+    Methods:
+        area: raises an exception
+    """
+
+    def area(self):
+        raise Exception("area() is not implemented")
