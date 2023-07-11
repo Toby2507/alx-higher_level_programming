43e3fb74054d4ef4fb63d092f697596aecd828a2
Toby2507 <oluwatobisalau2000@gmail.com>
2023-07-11 01:02:58 +0100

    Add a docstring to the lookup function

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -6,4 +6,9 @@ and methods of an object
 
 
 def lookup(obj):
+    """Computes the list of available attributes and methods
+
+    Returns:
+        list of available attributes and methods
+    """
     return dir(obj)
